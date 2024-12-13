69e35775386a9cddb9445640ea03ba45211b7c63
MrGreat-0 <jagjeetsinghrana2@gmail.com>
2024-12-13 19:41:02 +0530

    Update Button.jsx
    add user-select: none.

--- src/Componenets/Button.jsx
+++ src/Componenets/Button.jsx
@@ -3,7 +3,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 const Button = ({ title = "Get Started" }) => {
   return (
-    <div className="w-32 h-8 px-3 py-2 bg-zinc-100 text-black rounded-full text-center overflow-hidden group">
+    <div className="w-32 h-8 px-3 py-2 bg-zinc-100 text-black rounded-full text-center overflow-hidden group select-none">
       <div className="flex items-center justify-between relative transform group-hover:-translate-y-[150%] transition-transform duration-300 ease-custom-bezier">
         <span className="text-xs font-semibold">{title}</span>
         <IoIosReturnRight />
